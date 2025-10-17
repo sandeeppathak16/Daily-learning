f2404800e83839a399ec3fbf1e933dde6066417e
sandeeppathak16 <spathak1698@gmail.com>
2025-10-17 16:07:54 +0530

    format code

--- merge_sort/main.py
+++ merge_sort/main.py
@@ -1,40 +1,39 @@
 def merge_sort(list_l):
-  l = len(list_l)
+    l = len(list_l)
 
-  if l <=1:
-    return list_l
+    if l <= 1:
+        return list_l
 
-  mid = l//2
-  left = list_l[:mid]
-  right = list_l[mid:]
-  left = merge_sort(left)
-  right = merge_sort(right)
+    mid = l // 2
+    left = list_l[:mid]
+    right = list_l[mid:]
+    left = merge_sort(left)
+    right = merge_sort(right)
 
-  return merge(left, right)
-
-
+    return merge(left, right)
 
 
 def merge(left, right):
-  l = len(left)
-  r = len(right)
-  sorted_list = []
-  i=j=0
+    l = len(left)
+    r = len(right)
+    sorted_list = []
+    i = j = 0
 
-  while i<l and j<r:
-    if left[i]>right[j]:
-      sorted_list.append(right[j])
-      j+=1
-    else:
-      sorted_list.append(left[i])
-      i+=1
-  while i<l:
-    sorted_list.append(left[i])
-    i+=1
-  while j<r:
-    sorted_list.append(right[j])
-    j+=1
-  return sorted_list
+    while i < l and j < r:
+        if left[i] > right[j]:
+            sorted_list.append(right[j])
+            j += 1
+        else:
+            sorted_list.append(left[i])
+            i += 1
+    while i < l:
+        sorted_list.append(left[i])
+        i += 1
+    while j < r:
+        sorted_list.append(right[j])
+        j += 1
+    return sorted_list
+
 
 l = [2, 6, 11, 19, 27, 31, 45, 121]
 
