5109b1baf803fa3d377cfa0f536b6896f2820c15
sandeep <spathak1698@gmail.com>
2026-09-08 12:51:27 +0530

    aoc 2024 day 21 solution

--- AOC/2024/21.py
+++ AOC/2024/21.py
@@ -1,100 +1,152 @@
 from collections import deque
+from functools import cache
 
-def all_shortest_path(src, dest, grid, rows, cols):
-    queue = deque([(src, 0, '')])
-    visited = {src}
-    paths = {}
+NUMERIC = [
+    ["7", "8", "9"],
+    ["4", "5", "6"],
+    ["1", "2", "3"],
+    ["X", "0", "A"],
+]
 
-    mapping = {
-        (0, 1): '>',
-        (0, -1): '<',
-        (1, 0): 'v',
-        (-1, 0): '^'
-    }
+DIR = [
+    ["X", "^", "A"],
+    ["<", "v", ">"],
+]
 
-    while queue:
-        s, ct, p = queue.popleft()
+MOVES = [
+    (0, 1, ">"),
+    (0, -1, "<"),
+    (1, 0, "v"),
+    (-1, 0, "^"),
+]
 
-        r, c = s
+NUM_POS = {
+    "7": (0, 0), "8": (0, 1), "9": (0, 2),
+    "4": (1, 0), "5": (1, 1), "6": (1, 2),
+    "1": (2, 0), "2": (2, 1), "3": (2, 2),
+    "0": (3, 1), "A": (3, 2),
+}
 
-        if (r, c) == dest:
-            if not paths:
-                paths[ct] = [p + 'A']
-            elif paths and ct in paths:
-                paths[ct].append(p + 'A')
+DIR_POS = {
+    "^": (0, 1),
+    "A": (0, 2),
+    "<": (1, 0),
+    "v": (1, 1),
+    ">": (1, 2),
+}
 
-        for dr, dc in ((0, 1), (0, -1), (1, 0), (-1, 0)):
+
+def all_shortest_paths(start, end, grid):
+    rows, cols = len(grid), len(grid[0])
+
+    q = deque([(start, "")])
+    dist = {start: 0}
+
+    best = None
+    ans = []
+
+    while q:
+        pos, path = q.popleft()
+        r, c = pos
+
+        if best is not None and len(path) > best:
+            continue
+
+        if pos == end:
+            best = len(path)
+
+            if len(path) == best:
+                ans.append(path + "A")
+            continue
+
+        for dr, dc, move in MOVES:
             nr, nc = r + dr, c + dc
 
-            if (
+            if not (
                 0 <= nr < rows
                 and 0 <= nc < cols
                 and grid[nr][nc] != "X"
-                # and (nr, nc) not in visited
             ):
-                # visited.add((nr, nc))
-                new_path = p[:] + mapping[(dr, dc)]
-                queue.append(((nr, nc), ct + 1, new_path))
+                continue
 
-    return next(iter(paths.values()))
+            nd = len(path) + 1
 
+            if nd <= dist.get((nr, nc), float("inf")):
+                dist[(nr, nc)] = nd
+                q.append(((nr, nc), path + move))
 
-def solve(filename='21.txt'):
-    with open(filename, 'r') as f:
-        codes = [line.strip() for line in f.readlines()]
-
-    numeric_keypad_grid = [
-        ["7", "8", "9"],
-        ["4", "5", "6"],
-        ["1", "2", "3"],
-        ["X", "0", "A"]
-    ]
-
-    directional_keypad_grid = [
-        [None, "^", "A"],
-        ["<", "v", ">"]
-    ]
-
-    numeric_pos = {
-        "7": (0, 0), "8": (0, 1), "9": (0, 2),
-        "4": (1, 0), "5": (1, 1), "6": (1, 2),
-        "1": (2, 0), "2": (2, 1), "3": (2, 2),
-        "0": (3, 1), "A": (3, 2),
-    }
-
-    directional_pos = {
-        "^": (0, 1),
-        "A": (0, 2),
-        "<": (1, 0),
-        "v": (1, 1),
-        ">": (1, 2),
-    }
-
-    src = numeric_pos['A']
-    paths = []
-
-    for dest in '029A':
-        next_paths = all_shortest_path(src, numeric_pos[dest], numeric_keypad_grid, 4, 3)
-        print(src, numeric_pos[dest], next_paths, paths)
-
-        if not paths:
-            paths = next_paths
-        else:
-            new_paths = []
-
-            for path in paths:
-                for next_path in next_paths:
-                    new_paths.append(path + next_path)
-
-            paths = new_paths
-
-        src = numeric_pos[dest]
-
-    return paths
-
-print(solve())
+    return ans
 
 
+NUMERIC_PATHS = {}
+for a in NUM_POS:
+    for b in NUM_POS:
+        NUMERIC_PATHS[(a, b)] = all_shortest_paths(
+            NUM_POS[a], NUM_POS[b], NUMERIC
+        )
 
-    
+DIRECTIONAL_PATHS = {}
+for a in DIR_POS:
+    for b in DIR_POS:
+        DIRECTIONAL_PATHS[(a, b)] = all_shortest_paths(
+            DIR_POS[a], DIR_POS[b], DIR
+        )
 
+
+@cache
+def cost_between(a, b, depth):
+    paths = DIRECTIONAL_PATHS[(a, b)]
+
+    if depth == 0:
+        return min(len(p) for p in paths)
+
+    best = float("inf")
+
+    for path in paths:
+        total = 0
+        prev = "A"
+
+        for ch in path:
+            total += cost_between(prev, ch, depth - 1)
+            prev = ch
+
+        best = min(best, total)
+
+    return best
+
+
+def solve_code(code, robots=25):
+    total = 0
+    prev = "A"
+
+    for ch in code:
+        best = float("inf")
+
+        for path in NUMERIC_PATHS[(prev, ch)]:
+            cur = 0
+            p = "A"
+
+            for x in path:
+                cur += cost_between(p, x, robots - 1)
+                p = x
+
+            best = min(best, cur)
+
+        total += best
+        prev = ch
+
+    return total
+
+
+def solve(filename="21.txt", robots=25):
+    ans = 0
+
+    with open(filename) as f:
+        for code in map(str.strip, f):
+            presses = solve_code(code, robots)
+            ans += int(code[:-1]) * presses
+
+    return ans
+
+
+print(solve(robots=25))
